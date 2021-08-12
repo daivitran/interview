d88ddd8105b327f18c1eeee36a7f6657a7ff8735
David <vitrandao@gmail.com>
2021-08-12 15:28:57 -0700

    update knapsack with repetitions

--- classic/knapsack.py
+++ classic/knapsack.py
@@ -26,7 +26,27 @@ def knapsack_without_repetitions(weights, values, weight_limit):
 
     return dp[total_items - 1][weight_limit]
 
-
+# time: O(weight_limit * total_items)
+# space: O(weight_limit * total_items) -> can be reduced to O(weight_limit) using 1 dimensional array.
 def knapsack_with_repetitions(weights, values, weight_limit):
-    pass
+    total_items = len(weights)
 
+    # dp[i][j] is the max value that can be obtained using only items 0,..,i and total weight is less than j
+    dp = [[0 for smaller_weight_limit in range(weight_limit + 1)] for item_limit in range(total_items)]
+
+    # initialize first row
+    for smaller_weight_limit in range(weight_limit + 1):
+        multiple = smaller_weight_limit // weights[0]
+        dp[0][smaller_weight_limit] = values[0] * multiple
+
+    # the key is:
+    # dp[item_limit][smaller_weight_limit - weights[item_limit]] + values[item_limit]
+    for item_limit in range(1, total_items):
+        for smaller_weight_limit in range(1, weight_limit + 1):
+            value_with_new_item = 0
+            if weights[item_limit] <= smaller_weight_limit:
+                value_with_new_item = dp[item_limit][smaller_weight_limit - weights[item_limit]] + values[
+                    item_limit]
+            dp[item_limit][smaller_weight_limit] = max(dp[item_limit - 1][smaller_weight_limit], value_with_new_item)
+
+    return dp[total_items - 1][weight_limit]
